001570b221ddbbc118de81f76c3d06dc49135788
AntonZinchenko32 <zinchenko309@gmail.com>
2023-02-10 23:24:19 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,49 @@
-console.log("test1");
+console.log("test13499");
 
 import flatpickr from "flatpickr";
-
 import "flatpickr/dist/flatpickr.min.css"; 
+
+const startBtn = document.querySelector('[data-start]');
+const values = document.querySelectorAll('.value');
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    onClose(selectedDates) {
+      const currentUnixTime = new Date().getTime();
+      const unixTimeDifference = selectedDates[0].getTime() - currentUnixTime;
+        convertMs(unixTimeDifference);
   },
 };
 
-// Инициализация библтотеки 
 flatpickr("#datetime-picker", options);
+startBtn.addEventListener('click', startTimer);
+
+console.log(typeof(options.defaultDate))
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+function startTimer(event) {
+
+}
