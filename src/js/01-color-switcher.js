020e5349f87bc691f819e463eadc59d9501f0939
AntonZinchenko32 <zinchenko309@gmail.com>
2023-02-10 18:02:54 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+console.log("test122");
 
+const body = document.querySelector("body");
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+
+changeBtnState(stopBtn);
+
+startBtn.addEventListener("click", () => {
+    changeBtnState(startBtn);
+    
+    body.style.backgroundColor = getRandomHexColor();
+  
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    changeBtnState(stopBtn);
+});
+
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    changeBtnState(stopBtn);
+    changeBtnState(startBtn);
+});
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBtnState(btn) {
+    if (btn.hasAttribute("disabled")) btn.removeAttribute("disabled")
+    else btn.setAttribute("disabled", '');
+}
