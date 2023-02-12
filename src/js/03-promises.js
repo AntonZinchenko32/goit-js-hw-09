b5719c3bbc4a0479b625ad6e3e569420224d75a4
AntonZinchenko32 <zinchenko309@gmail.com>
2023-02-12 02:21:23 +0200

    Пишет первую рабочую версию скрипта для Задания 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+console.log("test12991");
+
+const form = document.querySelector('.form');
+
+
+form.addEventListener("submit", handleSubmit);
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+  
+  let {
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+
+  delay = Number(delay.value);
+  step = Number(step.value);
+  amount = Number(amount.value);
+
+  // Цикл створення промісів основуючись на введенних даних 
+  let initialDelay = delay;
+  for (let i = 1; i <= amount; i++) {
+    
+    createPromise(i, initialDelay)
+  
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+      
+    
+    initialDelay += step;
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  
+  return new Promise((resolve, reject) => {
+    const promiseValue = { position, delay };
+    setTimeout(() => {
+    if (shouldResolve) {
+      resolve(promiseValue);
+  } else {
+      reject(promiseValue);
+  }
+    }, delay);
+  });
+};
+  
+
